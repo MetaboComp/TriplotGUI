625889daf826b05e938395a0958795ff56f59fb1
Yingxiao Yan <yingxiao@net.chalmers.se>
2022-11-17 15:09:17 +0100

    ss

--- Casualgraph.R
+++ Casualgraph.R
@@ -20,10 +20,12 @@ Showdiagram<-function(exposure=NULL,
                       covariate_EC=NULL,
                       covariate_MC=NULL,
                       effect_modifier=NULL){
+  library(DiagrammeR)
+  library(diagram)
   a<-menu(c("Yes","No"),
           graphics = T,
           "Are you very clear about which variable is what (exposure, outcome, mediator, confounder or effect modifier)?")
-  if(a==2){stop("Please think about which variable ")}
+  if(a==2){stop("Please think about which variables are which ")}
   if(is.null(exposure)&is.null(outcome)){
 
   stop("must have exposure or outcome")
@@ -36,19 +38,23 @@ Showdiagram<-function(exposure=NULL,
 
 
 DiagrammeR::grViz("
-diagram=digraph {
-  graph [ranksep = 0.2]
+digraph {
 
-  node [shape = plaintext]
+  graph [
+       # ranksep = 0.2
+       nodestep=1]
+  node [shape = plaintext
+       fontname=Helvetica
+       penwidth=2.0]
     exposure    [label = 'Exposure',shape=box]
     outcome    [label = 'Outcome',shape=box]
-
     mediator  [label = 'Mediator',shape=box]
     effect_modifier [label = 'Effect modifier',shape=box]
     covariate_EM  [label = 'Covariate EM',shape=box]
     covariate_EO  [label = 'Covariate EO', shape = box]
-    covaraite_MO  [label = 'Covariate MO',shape=box]
-  edge [minlen = 2]
+    covariate_MO  [label = 'Covariate MO',shape=box]
+  edge [minlen = 2
+       arrowhead=diamond]
     exposure->outcome
 
     exposure->mediator->outcome
@@ -59,6 +65,38 @@ covariate_EM->exposure
 covariate_MO->mediator
 covariate_MO->outcome
 
-  { rank = same; A; Y }
+  { rank = same; exposure; outcome; mediator }
 }
 ")
+
+
+
+########################################################################################3
+library(diagram)
+data <- c(0, "'.47*'", 0,
+          0, 0, 0,
+          "'.36*'", "'.33* (.16)'", 0)
+M<- matrix (nrow=3,
+            ncol=3,
+            byrow = TRUE,
+            data=data)
+plot<- plotmat (M, pos=c(1,2),
+                name= c( "Math self-efficacy",
+                         "Math ability",
+                         "Interest in the math major"),
+                box.type = "rect",
+                box.size = 0.12,
+                box.prop=0.5,
+                curve=0)
+
+######################################################################
+
+
+
+
+
+
+
+
+
+}
